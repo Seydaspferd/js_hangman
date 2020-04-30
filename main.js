b224bb5c6da5811562d70e09418abdd03b85717c
Seydaspferd <damethe@hotmail.com>
2020-04-30 03:03:25 +0200

    close to finish

--- main.js
+++ main.js
@@ -23,20 +23,34 @@ for (var i = 0; i < word.length; i++) {
 }
 var remainingletters = word.length;
 
+var remaininglives = 6;
+
 while (remainingletters > 0) {
-  alert(answerarray.join(' '));
-  var guess = prompt('Guess a letter!');
-  if (guess === null) {
-    break;
-  } else if (guess.length !== 1) {
-    alert('Please enter a single letter.');
+  if (remaininglives < 1) {
+    alert('Out of lives, you lose');
   } else {
-    for (var j = 0; j < word.length; j++) {
-      if (answerarray[j] === '_') {
-        if (word[j] === guess) {
-          answerarray[j] = guess;
-          remainingletters--;
+    alert('Remaining lives: ' + remaininglives);
+    alert(answerarray.join(' '));
+    var guess = prompt('Guess a letter!');
+    if (guess === null) {
+      break;
+    } else if (guess.length !== 1) {
+      alert('Please enter a single letter.');
+    } else {
+      var guessedRightOnce = false;
+
+      for (var j = 0; j < word.length; j++) {
+        if (answerarray[j] === '_') {
+          if (word[j] === guess) {
+            guessedRightOnce = true;
+            answerarray[j] = guess;
+            remainingletters--;
+          }
         }
+      }
+
+      if (!guessedRightOnce) {
+        remaininglives--;
       }
     }
   }
---
+++ newmain.js
@@ -0,0 +1,76 @@
+var words = [
+  'car',
+  'air',
+  'fish',
+  'life',
+  'candy',
+  'firework',
+  'television',
+  'basketball',
+  'football',
+  'watercooler',
+  'computer',
+  'hangman',
+  'elephant',
+  'mathematic',
+  'radio',
+  'keyboard',
+];
+var word = words[Math.floor(Math.random() * words.length)];
+var answerarray = [];
+for (var i = 0; i < word.length; i++) {
+  answerarray[i] = '_';
+}
+var remainingletters = word.length;
+
+var remaininglives = 6;
+
+var imgcounter = 0;
+
+document.getElementById('answer').innerHTML = answerarray.join(' ');
+
+document.getElementById('remaininglives').innerHTML =
+  'Remaining lives: ' + remaininglives;
+
+document.getElementById('submitbtn').addEventListener('click', function () {
+  if (remainingletters > 0) {
+    if (remaininglives - 1 < 1) {
+      document.getElementById('losemessage').innerHTML = 'You lose';
+    } else {
+      document.getElementById('remaininglives').innerHTML =
+        'Remaining lives: ' + remaininglives;
+      document.getElementById('answer').innerHTML = answerarray.join(' ');
+      var guess = document.getElementById('guessinput').value;
+
+      var guessedRightOnce = false;
+
+      for (var j = 0; j < word.length; j++) {
+        if (answerarray[j] === '_') {
+          if (word[j] === guess) {
+            guessedRightOnce = true;
+            answerarray[j] = guess;
+            remainingletters--;
+            document.getElementById('answer').innerHTML = answerarray.join(' ');
+            if (remainingletters === 0) {
+              document.getElementById('losemessage').innerHTML =
+                'Good Job! The answer was: ' + word;
+            }
+          }
+        }
+      }
+    }
+  }
+
+  if (!guessedRightOnce && remaininglives !== 0) {
+    remaininglives--;
+
+    document.getElementById('remaininglives').innerHTML =
+      'Remaining lives: ' + remaininglives;
+
+    document
+      .getElementById('picturediv')
+      .children.item(imgcounter).style.visibility = 'visible';
+
+    imgcounter++;
+  }
+});
